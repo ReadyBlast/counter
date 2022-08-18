0fd3c1f1c7649b757ff663e2e088ceed7176eb79
Alexey Ovchinnikov <88533698+ReadyBlast@users.noreply.github.com>
2022-08-18 22:53:43 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
+import React from 'react';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+
+  const onClickPlus = () => {
+    setCounter(counter + 1);
+  };
+
+  const onClickMinus = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{counter}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
+      </div>
     </div>
   );
 }
